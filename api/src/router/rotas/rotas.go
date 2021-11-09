cb44cf4f26523737337fcef2a73a00e389d1df10
rslewenstein <rafasoaresjf@gmail.com>
2021-11-09 16:11:13 -0300

    Configurando as rotas

--- api/src/router/rotas/rotas.go
+++ api/src/router/rotas/rotas.go
@@ -1,11 +1,25 @@
 package rotas
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Representa todas as rotas da API
-type Rotas struct {
+type Rota struct {
 	URI                string
 	Metodo             string
 	Funcao             func(http.ResponseWriter, *http.Request)
 	RequerAutenticacao bool
+}
+
+// Coloca todas as rotas dentro do router
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+
+	for _, rota := range rotas {
+		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+	}
+	return r
 }

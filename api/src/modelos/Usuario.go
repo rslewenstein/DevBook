b4b55cbc0a64ef259bcea5d0764d703eda7e9070
rslewenstein <rafasoaresjf@gmail.com>
2022-07-21 23:38:07 -0300

    creating validation of Usuario struct

--- api/src/modelos/Usuario.go
+++ api/src/modelos/Usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 //representa um usuário utilizando a rede social
 type Usuario struct {
@@ -10,4 +14,40 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
+
+// Preparar vai chamar os métodos para validar e formatar o usuário recebido
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil{
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("o nome é obrigatório")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("o nick é obrigatório")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("o email é obrigatório")
+	}
+	
+	if usuario.Senha == "" {
+		return errors.New("a senha é obrigatório")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar(){
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }

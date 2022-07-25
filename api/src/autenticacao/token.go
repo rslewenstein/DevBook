bdd745f4e6144389891e3d5ef09d4a1ca0aca3ef
rslewenstein <rafasoaresjf@gmail.com>
2022-07-25 13:59:35 -0300

    creating token auth function

---
+++ api/src/autenticacao/token.go
@@ -0,0 +1,20 @@
+package autenticacao
+
+import (
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+// retorna um token assinado
+func CriarToken(usuarioID uint64) (string, error) {
+	permissoes := jwt.MapClaims{}
+	permissoes["authorized"] = true
+	// expiração do token (6h)
+	permissoes["exp"] = time.Now().Add(time.Hour * 6).Unix()
+	permissoes["usuarioId"] = usuarioID
+	// metodo de assinatura
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissoes)
+	// com o secret estamos assinando
+	return token.SignedString([]byte("Secret"))
+}

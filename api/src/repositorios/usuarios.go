0be379963acff02f3feaf6081f11ba6318e2db04
rslewenstein <rafasoaresjf@gmail.com>
2022-07-25 13:39:28 -0300

    creating login function

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -133,3 +133,22 @@ func (repositorio Usuarios) Deletar(ID uint64) error {
 
 	return nil
 }
+
+// busca um usuário por email e retona o seu id e senha com hash
+func (repositorio Usuarios) BuscarPorEmail(email string) (modelos.Usuario, error) {
+	linha, erro := repositorio.db.Query("select id, senha from usuarios where email = ?", email)
+	if erro != nil {
+		return modelos.Usuario{}, erro
+	}
+	defer linha.Close()
+
+	var usuario modelos.Usuario
+
+	if linha.Next() {
+		if erro = linha.Scan(&usuario.ID, &usuario.Senha); erro != nil {
+			return modelos.Usuario{}, erro
+		}
+	}
+
+	return usuario, nil
+}

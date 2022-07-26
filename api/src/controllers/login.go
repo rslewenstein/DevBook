fda6c9227040c18bd5fbb474241ba00a784815f2
rslewenstein <rafasoaresjf@gmail.com>
2022-07-25 21:55:07 -0300

    creating middlewares

--- api/src/controllers/login.go
+++ api/src/controllers/login.go
@@ -45,7 +45,11 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		respostas.Erro(w, http.StatusUnauthorized, erro)
 	}
 
-	token, _ := autenticacao.CriarToken(usuarioSalvoNoBanco.ID)
+	token, erro := autenticacao.CriarToken(usuarioSalvoNoBanco.ID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+	}
+
 	fmt.Println(token)
 	w.Write([]byte(token))
 }
---
+++ api/src/middlewares/middlewares.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// escreve informaões da requisição no terminal
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
+		next(w, r)
+	}
+}
+
+// verifica se o usuário está autenticado
+func Autenticar(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Autenticando...")
+		next(w, r)
+	}
+}
